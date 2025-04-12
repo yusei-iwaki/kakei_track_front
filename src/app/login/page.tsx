68fa1b098b7a53cce6407d8655d150b1558db4f6
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-04-12 13:42:56 +0900

    add:login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+"use client";
 
-const LoginPage: React.FC = () => {
+import { FC, FormEvent } from "react";
+import styles from "./page.module.scss";
+import button from "@/styles/button.module.scss";
+
+const LoginPage: FC = () => {
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault;
+    alert("ログイン処理");
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={styles.container_flex}>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <h1 className={styles.title}>ログインしてください</h1>
+          <form className={styles.form_container}>
+            <div className={styles.input_group}>
+              <label htmlFor="email">メールアドレス</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="example@example.com"
+                required
+              />
+            </div>
+            <div className={styles.input_group}>
+              <label htmlFor="password">パスワード（8文字以上）</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="8文字以上のパスワード"
+                required
+              />
+            </div>
+            <div className={styles.input_button}>
+              <button
+                onClick={handleLogin}
+                className={button.button_design1}
+                type="submit"
+              >
+                会員登録
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
