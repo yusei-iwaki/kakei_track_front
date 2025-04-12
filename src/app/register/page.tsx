ab56a3eff07ceb0ed4a6fcce4fd4d767a780a3f6
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-04-12 13:33:08 +0900

    fix:register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+"use client";
+
+import { FC, FormEvent } from "react";
 import styles from "./page.module.scss";
 import button from "@/styles/button.module.scss";
 
-const RegisterPage: React.FC = () => {
+const RegisterPage: FC = () => {
+  const handleRegister = (e: FormEvent) => {
+    e.preventDefault;
+    alert("会員登録処理");
+  };
   return (
     <div className={styles.container_flex}>
       <div className={styles.container}>
@@ -11,18 +17,40 @@ const RegisterPage: React.FC = () => {
           <form className={styles.form_container}>
             <div className={styles.input_group}>
               <label htmlFor="name">ユーザー名</label>
-              <input type="text" id="name" name="name" required />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="山田太郎"
+                required
+              />
             </div>
             <div className={styles.input_group}>
               <label htmlFor="email">メールアドレス</label>
-              <input type="email" id="email" name="email" required />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="example@example.com"
+                required
+              />
             </div>
             <div className={styles.input_group}>
               <label htmlFor="password">パスワード（8文字以上）</label>
-              <input type="password" id="password" name="password" required />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="8文字以上のパスワード"
+                required
+              />
             </div>
             <div className={styles.input_button}>
-              <button className={button.button_design1} type="submit">
+              <button
+                onClick={handleRegister}
+                className={button.button_design1}
+                type="submit"
+              >
                 会員登録
               </button>
             </div>
